26950fcee29a4d45f57dfbf5befd5d1439844c8a
Ingrid <mtra.ingrid.lopez@gmail.com>
2024-07-16 14:09:16 -0700

    Se agregaron los archivos del proyecto

---
+++ index.js
@@ -0,0 +1,9 @@
+function getAgeByBirthday(Birthday){
+    const today = new Date()
+    today.getFullYear()
+    const myBday= new Date("")
+    
+    myBday.getFullYear() - myBday.getFullYear()
+    
+
+}
